c8793f3f8b641c0c171cd940de89c5ff985c7640
saltowl <vladislava-shakirova@mail.ru>
2018-02-16 15:50:41 +0300

    Add google search for word frow TextView

--- app/src/main/java/saltowl/firsttask/SecondActivity.java
+++ app/src/main/java/saltowl/firsttask/SecondActivity.java
@@ -1,10 +1,15 @@
 package saltowl.firsttask;
 
+import android.app.SearchManager;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.StringRes;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity
 {
@@ -17,8 +22,31 @@ public class SecondActivity extends AppCompatActivity
         @Override
         public void onClick(View view)
         {
+            invokeWebSearch();
         }
     };
+
+    public void invokeWebSearch()
+    {
+        try
+        {
+            Bundle bundle = getIntent().getExtras();
+            String q = bundle.getString(SOME_KEY);
+            Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+            intent.putExtra(SearchManager.QUERY, q);
+            startActivity(intent);
+        }
+        catch (ActivityNotFoundException e)
+        {
+            showMessage(R.string.error_browser);
+            e.printStackTrace();
+        }
+    }
+
+    private void showMessage(@StringRes int string)
+    {
+        Toast.makeText(this, string, Toast.LENGTH_LONG).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
